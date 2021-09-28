6cbcd1fd702d4dbd645878c30f5229e9d4d264ae
jhamadhav <jhamadhav28@gmail.com>
2021-09-28 23:43:03 +0530

    quote added

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
+const http = require('https');
+// const fetch = require('node-fetch')
 
-fs.writeFile("test.txt", "Hey there!", function (err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});
+const init = async () => {
+    http.request("https://quotable.io/random", function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            data = JSON.parse(chunk)
+
+            let txtData = `${data.content}\n-${data.author}`
+            console.log(txtData)
+
+            fs.writeFile("test.txt", txtData, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+
+        });
+    }).end();
+
+
+}
+
+init()
+
