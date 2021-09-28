27b35c764597a19056b31f8aa201bbf0d2d589fa
jhamadhav <jhamadhav28@gmail.com>
2021-09-29 01:25:41 +0530

    quotes updated in readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# New Quotes via Github action and Cron Job
+
+<pre>
+<!-- #quote -->
+Happiness depends upon ourselves.
+- Aristotle
+
+Updated on: [29/9/2021]
+<!-- #quoteEnd -->
+</pre>
--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const http = require('https');
 // const fetch = require('node-fetch')
 
 const init = async () => {
+
+    let readData = fs.readFileSync('README.md', 'utf8')
+
     http.request("https://quotable.io/random", function (res) {
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             data = JSON.parse(chunk)
+            console.log(data);
 
-            let txtData = `${data.content}\n-${data.author}`
-            console.log(txtData)
 
-            fs.writeFile("test.txt", txtData, function (err) {
+            let currentTime = new Date();
+            let currentOffset = currentTime.getTimezoneOffset();
+            let ISTOffset = 330;   // IST offset UTC +5:30 
+            let d = new Date(currentTime.getTime() + (ISTOffset + currentOffset) * 60000);
+            d = d.toLocaleDateString("en-IN")
+
+            readData = readData.replace(/<!-- #quote -->(.|\n)*<!-- #quoteEnd -->/gm, `<!-- #quote -->\n${data.content}\n- ${data.author}\n\nUpdated on: [${d}]\n<!-- #quoteEnd -->`)
+
+            console.log(readData)
+
+            fs.writeFile("README.md", readData, function (err) {
                 if (err) {
                     return console.log(err);
                 }
